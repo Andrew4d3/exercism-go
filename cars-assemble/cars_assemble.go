606bf52afc0b0f630d602afda9b8bce3f3ad096c
Andrew Davis <andrew4d3@gmail.com>
2025-10-04 17:39:25 -0300

    New exercises done

--- cars-assemble/cars_assemble.go
+++ cars-assemble/cars_assemble.go
@@ -3,16 +3,19 @@ package cars
 // CalculateWorkingCarsPerHour calculates how many working cars are
 // produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	panic("CalculateWorkingCarsPerHour not implemented")
+	return float64(productionRate) * (successRate / 100)
 }
 
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	panic("CalculateWorkingCarsPerMinute not implemented")
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate)) / 60
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	panic("CalculateCost not implemented")
+	maxGroup := (carsCount / 10) * 95_000
+	leftoverGroup := carsCount % 10 * 10_000
+
+	return uint(maxGroup + leftoverGroup)
 }
